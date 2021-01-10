63f022fc2c348e474d12c43db7ad619f3315b0a0
Tolu Adesegha <tolu@adesegha.ca>
2021-01-10 12:30:50 -0700

    synchronizing repositories

--- scripts/synchronize-repositories.sh
+++ scripts/synchronize-repositories.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/bash
 
 for folder in ~/dotfiles ~/tools; do
-	cd $folder && git pull && git add -A && git commit -m 'synchronizing repositories' && git push
+	cd $folder && git add -A && git commit -m 'synchronizing repositories' && git pull && git push
 done
